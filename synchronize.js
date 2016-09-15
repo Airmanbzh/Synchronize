9a16aa6b899d87d59f0f856e0c77cc9c73ed42a4
Le Boulch Brice <bleboulch@voyages-sncf.com>
2016-09-15 13:40:39 +0200

    Add when condition

--- synchronize.js
+++ synchronize.js
@@ -3,88 +3,171 @@
  * @class Synchronize
  * @description synchronize functions to execute one after the others
  */
-Synchronize = function() {
-    /**
-     * List of currents functions to synchronize
-     * @type {Array}
-     */
-    this.functions = [];
+Synchronize = function ()
+{
+	/**
+	 * List of currents functions to synchronize
+	 * @type {Array}
+	 */
+	this.functions = [];
 
-    /**
-     * Start automatically (or not) when you push a new function
-     * @type {Boolean}
-     */
-    this.autostart = true;
+	/**
+	 * List of currents functions which wait other to finish
+	 * @type {Array}
+	 */
+	this.conditionalFunctions = [];
 
-    /**
-     * Id of the last function inserted
-     * @type {Number}
-     */
-    this.lastId = 0;
+	/**
+	 * Start automatically (or not) when you push a new function
+	 * @type {Boolean}
+	 */
+	this.autostart = true;
 
-    /**
-     * Wait (or not) notification from current execution before execute next function
-     * @type {Boolean}
-     */
-    this.wait_notification = false;
+	/**
+	 * Id of the last function inserted
+	 * @type {Number}
+	 */
+	this.lastId = 0;
 
-    /**
-     * Add a function to the queue
-     * @param function f    function to execute
-     * @param array args    arguments
-     * @return {Number}     identification number
-     */
-    this.push = function(f, args) {
-        this.lastId = this.lastId + 1 || new Date().getTime();
-        this.functions.push({id: this.lastId, _function:f, _args:args,state:0, value:undefined});
-        if (this.autostart) {this.start();}
-        return this.lastId;
-    };
+	/**
+	 * Wait (or not) notification from current execution before execute next function
+	 * @type {Boolean}
+	 */
+	this.waitNotification = true;
 
-    /**
-     * @description Notify that the current function is finished and start the next one
-     * @param (optionnal)id ID of the function to notify as stopped/finished
-     */
-    this.notify = function(id) {
-        var find = false;
-        for (var i= 0, ln = this.functions.length, find = false; i < ln && find == false; i++) {
-        	if ((this.functions[i]._function == this.notify.caller && this.functions[i].state == 1)
-            || this.functions[i].id == id) {
-                this.functions[i].state = -1;
-                find = true;
-            }
-        }
-        if (find && (this.autostart || this.wait_notification)) {this.start();}
-        return find;
-    };
+	/**
+	 * Add a function to the queue
+	 * @param function f    function to execute
+	 * @return {Number}     identification number
+	 */
+	this.push = function (f)
+	{
+		addFunction(
+			{
+				_function: f,
+				_args:Array.from(arguments).slice(1)
+			}
+		);
 
-    /**
-     * @description Start the execution
-     */
-    this.start = function() {
-        for (var i= 0, ln = this.functions.length, find = false; i < ln && find == false; i++) {
-            if (this.functions[i].state == 0) {
-                this.functions[i].state = 1;
-                this.functions[i].value = this.functions[i]._function(this.functions[i]._args);
-                find = true;
-            } else if (this.functions[i].state == 1) {
-                find = true;
-            }
-        }
-        if (!this.wait_notification) {this.notify();}
-    };
+		if (this.autostart)
+		{
+			startFunction(this.functions.length - 1);
+		}
+		
+		return this.lastId;
+	};
 
-    /**
-     * @description Stop the execution
-     */
-    this.stop = function() {
-        this.autostart=false;
-    };
+	/**
+	 * Add a function to the queue with conditional parameters
+	 * @param {Array} Array of IDs to wait
+	 * @param function f    function to execute
+	 * @return {Number}     identification number
+	 */
+	this.when = function (wait, f)
+	{	
+		addFunction(
+			{
+				wait:wait,
+				_function: f,
+				_args:Array.from(arguments).slice(2)
+			}
+		);
+	};
+	
+	var addFunction = function(f)
+	{
+		this.lastId = this.lastId + 1 || new Date().getTime();
+		
+		f.id = this.lastId;
+		f.state = 0;
+		f.value = undefined;
+		f.wait = f.wait || [];
+		
+		this.functions.push(f);
+		
+		return this.lastId;
+	}.bind(this);
 
-    /**
-     * @description Reverse functions order
-     */
-    this.reverse = function() {
-    	this.functions.reverse();
-    };
+	/**
+	 * @description Notify that the current function is finished and start the next one
+	 * @param (optionnal)id ID of the function to notify as stopped/finished
+	 */
+	this.notify = function (id)
+	{
+		var find = false;
+		for (var i = 0, ln = this.functions.length, find = false; i < ln && find === false; i++)
+		{
+			if ((this.functions[i]._function === this.notify.caller && this.functions[i].state === 1)
+					|| this.functions[i].id === id)
+			{
+				this.functions[i].state = -1;
+				find = true;
+			}
+		}
+		
+		if (find && (this.autostart || this.waitNotification))
+		{
+			this.start();
+		}
+		return find;
+	};
+
+	/**
+	 * @description Start the execution
+	 */
+	this.start = function ()
+	{
+		for (var i = 0, ln = this.functions.length, find = false; i < ln && find === false; i++)
+		{
+			if (this.functions[i].state === 0)
+			{
+				find = !this.functions[i].wait.length;
+				if (!find)
+				{
+					var ready = true;
+					this.functions[i].wait.forEach(function(v,k){
+						ready &= this.functions[v - 1].state === -1;
+					}.bind(this));
+					
+					find = ready;
+				}
+				
+				if (find)
+				{
+					startFunction(i);
+				}
+			}
+			else if (this.functions[i].state === 1)
+			{
+				find = true;
+			}
+		}
+		
+		if (!this.waitNotification)
+		{
+			this.notify();
+		}
+	};
+	
+	var startFunction = function(i)
+	{
+		this.functions[i].state = 1;
+		this.functions[i].value = this.functions[i]._function(this.functions[i]._args);
+	}.bind(this);
+
+	/**
+	 * @description Stop the execution
+	 */
+	this.stop = function ()
+	{
+		this.autostart = false;
+	};
+
+	/**
+	 * @description Reverse functions order
+	 */
+	this.reverse = function ()
+	{
+		this.functions.reverse();
+	};
 };
